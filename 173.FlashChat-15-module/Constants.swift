24e55e75b40fab2d5bc53aaeb6ff987d9026d420
latronixo <pamerra@mail.ru>
2025-03-01 00:02:02 +0300

    Удалил сториборд, настроил navigationController

---
+++ 173.FlashChat-15-module/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  173.FlashChat-15-module
+//
+//  Created by Валентин Картошкин on 01.03.2025.
+//
+
+import Foundation
---
+++ 173.FlashChat-15-module/WelcomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  173.FlashChat-15-module
+//
+//  Created by Валентин Картошкин on 28.02.2025.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    // MARK: - UI
+    
+    // MARK: - Life Cycle
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        setViews()
+        setupConstraints()
+    }
+
+    // MARK: - Set Views
+    
+    private func setViews() {
+        view.backgroundColor = .white
+
+    }
+}
+
+// MARK: - Setup Constraints
+ 
+extension WelcomeViewController {
+    
+    private func setupConstraints() {
+        
+    }
+}
