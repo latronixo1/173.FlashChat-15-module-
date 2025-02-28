f184de10b405aea0d43e21d5420abff3c15d2f5f
latronixo <pamerra@mail.ru>
2025-02-28 23:40:52 +0300

    Initial Commit

---
+++ 173.FlashChat-15-module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  173.FlashChat-15-module
+//
+//  Created by Валентин Картошкин on 28.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
